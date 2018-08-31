5ed5ae42bab0f8524d5ed1b0027780e1a60dc01a
ijh5005 <ijh5005@outlook.com>
2018-08-31 13:51:48 -0400

    Add files via upload

---
+++ reactNotes.js
@@ -0,0 +1,109 @@
+// the main component the bring in the other components
+import React, { Component } from 'react';
+import Provider from './context';
+import Contact from './contact';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div>
+          <Contact />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
+
+// contact.js
+import React, { Component } from 'react'
+import Consumer from 'context';
+
+class Contact extends Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { variable } = value;
+          return (
+            <div>
+              {variable}
+            </div>
+          )
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Contact;
+
+// context.js
+import React, { Component } from 'react'
+const Context = React.createContext();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SOME_ACTION':
+      return {
+        ...state,
+        somethingInState: state.somethingInState.filter(data => { data !== action.payload })
+      }
+      break;
+    case 'ANOTHER_ACTION'
+      return {
+        ...state,
+        someArray: [...state.someArray, action.payload]
+      }
+    default:
+      return state
+
+  }
+}
+
+export class Provider extends Component {
+  state = {
+    something: 'something',
+    dispatch: action => {
+      this.setState(state => {
+        reducer(state, action)
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
+
+
+//imputs
+...
+state = {
+  name: 'isaiah harrison',
+  email: 'isaiahharrison14@gmail.com'
+}
+onSubmit = () => {
+  e.preventDefault();
+  ...
+}
+onChange = (e) => {
+  this.setState({ [e.target.name]: e.target.value })
+}
+...
+render(){
+  const {name, email} = this.state;
+  return (
+    <form onSubmit={this.onSubmit}>
+      <input name='name'  value={name}  onChange={this.onChange}/>
+      <input name='email' value={email} onChange={this.onChange}/>
+    </form>
+  )
+}
